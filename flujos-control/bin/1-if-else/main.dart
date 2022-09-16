0e30db3a2d4f249d3efaa858a079a99abafe84d4
Sergio Ochoa Martinez <gnu.java.sergio@gmail.com>
2022-09-15 20:38:05 -0400

    ejemplos clase 3
    fix #5

---
+++ flujos-control/bin/1-if-else/main.dart
@@ -0,0 +1,10 @@
+void main() {
+  int age = 25;
+  if (age == 18) {
+    print("Puedes votar, sera tu 1ra votacion");
+  } else if (age > 18) {
+    print("Puedes votar de nuevo");
+  } else {
+    print("Aun no puedes votar");
+  }
+}
